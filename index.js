41d802130cc2b1915cb30df801b96214df02f9b6
unknown <patrajuanda10@gmail.com>
2024-01-06 21:53:02 +0700

    update cache api and limiter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-
+const rateLimiter = require('express-rate-limit')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 
+// rate limiter 
+const limiter =rateLimiter({
+    windowMs:10 * 60 * 1000,
+    max :10
+})
+app.use(limiter)
+app.set('trust proxy',1)
 
 app.use('/api', require('./routes'))
 
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,31 @@ const express = require("express");
 
 const needle = require("needle");
 
+const apicache = require('apicache')
 const router = express.Router();
 // env var
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
+//init api cache
 
+let cache = apicache.middleware
 
-router.get("/", async (req, res) => {
+router.get("/", cache('2 minutes'), async (req, res) => {
   try {
     const appid = API_KEY_VALUE;
     const query = req.query.q;
-
+    
     const apiRes = await needle(
       "get",
       `https://api.openweathermap.org/data/2.5/weather?appid=${appid}&q=${query}`
     );
     const data = apiRes.body;
+
+    if(process.env.NODE_ENV !== 'production'){
+      console.log(`REQUEST :https://api.openweathermap.org/data/2.5/weather?appid=${appid}&q=${query} `)
+    }
     res.status(200).json(data);
 
   } catch (error) {
